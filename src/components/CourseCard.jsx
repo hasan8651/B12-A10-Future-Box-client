6a0b31f9831608a4cae5e85dc6ebc30e892e5f17
hasan8651 <hasan865@gmail.com>
2025-11-08 23:25:14 +0600

    Popular Courses data fetching from mongoDB

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { Link } from 'react-router';
 
 const CourseCard = ({ course }) => {
-   const {_id, title, imageURL, price, duration, category } = course
-
+const {_id, title, imageURL, price, duration, category} = course
 
   return (
     <div data-aos="fade-up">
@@ -19,7 +17,7 @@ const CourseCard = ({ course }) => {
           </span>
           <p className='text-md my-2 font-semibold text-center text-blue-500'>Category: <span className='text-green-600'> {category}</span></p>
         </div>
-        <Link to={`/course/${_id}`} className="btn h-14 bg-blue-500 text-white hover:bg-blue-700">View More</Link>
+        <Link to={`/course/${_id}`} className="btn h-14 bg-blue-500 text-white hover:bg-blue-700">View Details</Link>
       </div>
     </div>
   );
